1a7562dd7e087b47c6bfcedd7e85cb3652921bc0
M0hamed-Gamall <jimmy.mans00ur@gmail.com>
2024-12-20 23:50:10 +0200

    make validation array to userinput using express-validator

--- controllers/auth.controller.ts
+++ controllers/auth.controller.ts
@@ -1,10 +1,29 @@
 import { RequestHandler } from "express";
 import {emailExist , addUser} from '../models/auth.model'
+import { body , validationResult} from 'express-validator'
+
+export const validateUser = [
+    body('username').notEmpty().withMessage("username is required"),
+    body("email").isEmail().withMessage("enter a correct email"),
+    body("confirm-password").custom((value,{req}) => {
+        if(value !== req.body.password){
+            throw new Error("passwords don't match")
+        }
+        return true;
+    }),
+    body("email").custom(async (value )=>{
+        if(await emailExist(value)){
+            throw new Error("email is already exists")
+        }
+        return true;
+    })
+]
 
 export const signup:RequestHandler = async (req,res,next)=>{
     // check if data is valid and send errors with express-flash
     // check if the email is unique
     //add data to database
 
+  
     
 }
--- routes/auth.route.ts
+++ routes/auth.route.ts
@@ -1,12 +1,12 @@
 import { Router } from "express";
-import { signup } from "../controllers/auth.controller";
+import { signup , validateUser } from "../controllers/auth.controller";
 
 const router = Router();
 
 router.get('/signup' , (req,res,next)=>{
     res.render('signup');
 })
-router.post('/signup' , signup)
+router.post('/signup' ,validateUser ,signup)
 
 
 export default router;
